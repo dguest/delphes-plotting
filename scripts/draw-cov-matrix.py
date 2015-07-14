f49602c60fc23445bf64791cb3373f123544f03f
Dan Guest <dguest@cern.ch>
2015-07-13 21:42:58 -0700

    more draw tweaks

--- scripts/draw-cov-matrix.py
+++ scripts/draw-cov-matrix.py
@@ -16,7 +16,19 @@ import json
 import warnings
 from enum import Enum
 
-_parameter = Enum('parameter', 'd0 z0 phi theta qoverp')
+class _trkpar(Enum):
+    d0 = (0, r'$d_0$', 'mm')
+    z0 = (1, r'$z_0$', 'mm')
+    phi= (2, r'$\phi$', '')
+    theta = (3, r'$\theta$', '')
+    qoverp = (4, r'$q/p$', r'MeV$^{-1}$')
+    def __init__(self, index, string, units):
+        self.index = index
+        self.string = string
+        self.units = units
+    # def __len__(self):
+    #     return len(__class__.__members__)
+
 
 def _get_args():
     parser = argparse.ArgumentParser()
@@ -30,12 +42,14 @@ def run():
     with open(args.input_file, 'r') as jfile:
         cov_pars = _int_keys(json.load(jfile))
     _print_cor_matrix(cov_pars, 0, 0, output_dir=args.output_dir)
-    d0d0_cov = _get_par_eta_vs_pt(cov_pars, _parameter.d0)
-    _print_par_eta_vs_pt(d0d0_cov, output_dir=args.output_dir)
+    for par in [_trkpar.d0, _trkpar.z0, _trkpar.phi, _trkpar.theta, _trkpar.qoverp]:
+        cov = _get_par_eta_vs_pt(cov_pars, par)
+        _print_par_eta_vs_pt(cov, output_dir=args.output_dir,
+                             parameter=par)
 
 _pt_bins = dict(enumerate([10, 20, 50, 100, 200, 250, 500, 750]))
 _eta_bins = dict(enumerate([0.0, 0.4, 0.8, 1.05, 1.5, 1.7, 2.0, 2.25, 2.7]))
-_pretty_labels=[r'$d_0$', r'$z_0$', r'$\phi$', r'$\theta$', r'$q/p$']
+
 def _int_keys(json_dict):
     """recursive translation to int keyed dict"""
     try:
@@ -44,10 +58,10 @@ def _int_keys(json_dict):
         return json_dict
     return odict
 
-def _label_ticks(axis, labels=_pretty_labels):
+def _label_ticks(axis, labels=_trkpar):
     tickpos = np.linspace(0, len(labels) - 1, len(labels))
     axis.set_ticks(tickpos)
-    axis.set_ticklabels(labels)
+    axis.set_ticklabels([x.string for x in labels])
 
 def _print_cor_matrix(matlist, pt_bin, eta_bin, output_dir):
     ar = np.asarray(matlist[pt_bin][eta_bin])
@@ -58,9 +72,9 @@ def _print_cor_matrix(matlist, pt_bin, eta_bin, output_dir):
     ax = fig.add_subplot(1,1,1)
     cmap = get_cmap('RdBu')
     lin_order = 3               # make log flat after this
-    norm = SymLogNorm(linthresh=10**(-lin_order), vmin=-1.001, vmax=1.001)
+    norm = SymLogNorm(linthresh=10**(-lin_order), vmin=-1.001, vmax=1-0.001)
     img = ax.matshow(np.asarray(cov), norm=norm, cmap=cmap)
-    cmap.set_under('k')
+    # cmap.set_over('k')
     _label_ticks(ax.xaxis)
     _label_ticks(ax.yaxis)
     ticks = 0.1**np.r_[lin_order:0:-1]
@@ -75,7 +89,7 @@ def _print_cor_matrix(matlist, pt_bin, eta_bin, output_dir):
     canvas.print_figure(os.path.join(output_dir,outname), bbox_inches='tight')
 
 
-def _print_par_eta_vs_pt(cov_array, output_dir):
+def _print_par_eta_vs_pt(cov_array, output_dir, parameter):
     fig = Figure(figsize=(5.0, 5.0*3/4))
     canvas = FigureCanvas(fig)
     ax = fig.add_subplot(1,1,1)
@@ -93,13 +107,17 @@ def _print_par_eta_vs_pt(cov_array, output_dir):
     cmap.set_under('k')
     img = ax.pcolormesh(zeroed_pt, zeroed_eta, stdiv_array.T, vmin=0,
                         cmap=cmap)
-    cb = fig.colorbar(img, label='$d_0$ width [mm]', )
+    lab = parameter.string + ' width'
+    units = parameter.units
+    if units:
+        lab += ' [{}]'.format(units)
+    cb = fig.colorbar(img, label=lab)
     # img.set_under('k')
     ax.set_xscale('log')
     ax.set_xlim(5, zeroed_pt[-1])
     ax.set_xlabel(r'$p_{\rm T}$ [GeV]', x=0.98, ha='right')
     ax.set_ylabel(r'$|\eta|$', y=0.98, ha='right')
-    outname = 'eta_vs_pt.pdf'
+    outname = '{}_eta_vs_pt.pdf'.format(parameter.name)
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
     canvas.print_figure(os.path.join(output_dir,outname), bbox_inches='tight')
@@ -107,7 +125,7 @@ def _print_par_eta_vs_pt(cov_array, output_dir):
 
 def _get_par_eta_vs_pt(cov_pars, parameter):
     pt_eta_array = np.ones((len(_pt_bins), len(_eta_bins))) * -1
-    par = parameter.value
+    par = parameter.index
     for pt_bin, pt_upper in _pt_bins.items():
         for eta_bin, eta_upper in _eta_bins.items():
             # spt, seta = str(pt_bin), str(eta_bin)
@@ -115,6 +133,8 @@ def _get_par_eta_vs_pt(cov_pars, parameter):
             if not pt_eta_list: continue
             pt_eta_array[pt_bin, eta_bin] = pt_eta_list[par][par]
     return pt_eta_array
+
+# ________________________________________________________________________
 
 def _log_formatting(value, pos):
     if value == 0:

a72f3153d24905e7c9ca6691579aae71dc38ec8c
Dan Guest <dguest@cern.ch>
2015-09-17 06:42:14 -0700

    prettier axis labels

---
+++ python/delphes/varnames.py
@@ -0,0 +1,16 @@
+labels = {
+    'jetProb': 'JetProb',
+    'lsig': 'SV sig',
+    'track2d0': r'2nd $d_0$',
+    'track3d0': r'3rd $d_0$',
+    'drjet': r'$\Delta R(\mathrm{jet}, \mathrm{vx})$',
+    'efrc': r'Sec Vx Energy Fraction',
+    'efrac': r'Sec Vx Energy Fraction',
+    'mass': r'Vertex Mass',
+    'n_sig_track': r'$n$ track $S_{L} > 1.8$',
+    'n_vx_track': r'$n$ Vx Matched Tracks',
+    'nsecvtx': r'$n$ Secondary Vertex',
+    'width_eta': r'width $\eta$',
+    'width_phi': r'width $\phi$',
+    'dphi': r'$\Delta \phi(\mathrm{jet}, \mathrm{vx})$'
+}
